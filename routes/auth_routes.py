4e352ac054ff0e1fcc87b87ed292d6e520923e6f
lewis wanjohi <i.johilewis@gmail.com>
2026-02-02 22:01:58 +0300

    did the register authentication and login auth logic

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -18,7 +18,7 @@ register_parser.add_argument("email", type=str,required=True, help="Email is req
 register_parser.add_argument("password",type=str ,required=True, help="Password is required")
 register_parser.add_argument("phone", type=str,required=False)
 
-
+#POST /auth/register → show messages auto-login.
 class RegisterResource(Resource):
     def post(self):
         try:
@@ -60,3 +60,55 @@ class RegisterResource(Resource):
             print(str(e))
             return {"message": "Missing Values", "error": "IntegrityError"}, 422
 
+#POST /auth/login → store access_token, user.role.
+class LoginResource(Resource):
+    def post(self):
+        data = register_parser.parse_args()
+
+        email = data.get("email")
+        password = data.get("password")
+
+        if not all([email, password]):
+            return jsonify({"error": "email and password are required"}), 400
+        
+        user = User.query.filter_by(email=data['email']).first()
+        if not user.is_active:
+            return jsonify ({
+                "error": "Account is disabled"
+            }), 403
+
+
+        if user and user.check_password(data['password']):
+            access_token = create_access_token(identity=user)
+            refresh_token = create_refresh_token(identity=user)
+
+            return jsonify(
+                {
+                    "message": "Login successful",
+                    "user": user.to_dict(),
+                    "access_token": access_token,
+                    "refresh_token": refresh_token,
+                }
+            ),200
+        return jsonify({"error": "Invalid email or password"}), 401
+    
+
+class MeResource(Resource):
+    @jwt_required()
+    def get(self):
+        user_id = get_jwt_identity()
+
+        user = User.query.get(user_id)
+
+        if not user:
+            return {"message": "User not found"}, 404
+
+        return {
+            "id": user.id,
+            "email": user.email,
+            "full_name": user.full_name,
+            "phone": user.phone,
+            "role": user.role,
+            "is_active": user.is_active,
+            "created_at": user.created_at.isoformat()
+        }, 200
